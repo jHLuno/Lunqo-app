09ef82d2cf908ed9be565e285a26c8b7ecfbac9a
xezii <amirzhan.zhampeisov01@gmail.com>
2025-07-31 22:33:50 +0500

    Favicon upd

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: 'index.html',
+      favicon: './public/Lunqo.svg',
     }),
   ],
   devServer: {
