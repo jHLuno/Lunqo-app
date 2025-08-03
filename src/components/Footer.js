1a424a4f05989d7805e1295c67c878d07ccfeab0
xezii <amirzhan.zhampeisov01@gmail.com>
2025-08-03 22:56:15 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,16 +43,6 @@ const Footer = () => {
 
   return (
     <footer id="footer" className="relative bg-black">
-      {/* Gradient transition from testimonials */}
-      {/*
-      <div className="
-    absolute inset-x-0 top-0            pin to top, stretch full width  
-    h-[100px]                            exactly 100 px high             
-    pointer-events-none                  never block clicks underneath   
-    bg-[linear-gradient(to_bottom,rgba(0,0,0,0.10)_0%,rgba(0,0,0,1)_100%)]" />
-      */}
-     
-      
       <div className="relative z-10 container-custom py-16 px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
           {/* Brand Section */}
